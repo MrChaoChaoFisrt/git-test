4f898caca621279a01ff4d809737d9ac6dabbc53
konglc <chao@inovance.com>
2022-05-21 22:24:46 +0800

    chao second commit

---
+++ src/main/java/com/chao/gitTest.java
@@ -0,0 +1,19 @@
+package com.chao;
+
+/**
+ * @author Mr chaochao
+ * @version 1.0
+ * @date 2022/5/21 22:20
+ */
+
+/**
+ * 添加到临时区
+ * 绿色的文件都是被git追踪到的
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        // 代码修改后 文件就会变蓝
+        System.out.println("Hello Git2");
+    }
+}
